0e43e05b0fda8e8ecc79d92bdf63d653b6417b13
Francu Alexandru Lucian <alexandru.lucian89@gmail.com>
2015-05-16 19:53:55 +0100

    Work
    New date picker in activity_work

--- app/src/main/java/com/mycompany/inout/WorkActivity.java
+++ app/src/main/java/com/mycompany/inout/WorkActivity.java
@@ -1,39 +1,77 @@
 package com.mycompany.inout;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class WorkActivity extends Activity {
-    protected EditText mStartDate;
-    protected EditText mEndDate;
-    protected Button mCalculateBtn;
+public class WorkActivity extends Activity implements OnClickListener {
+
+    //UI References
+    private EditText fromDateEtxt;
+    private EditText toDateEtxt;
+
+    private DatePickerDialog fromDatePickerDialog;
+    private DatePickerDialog toDatePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
-        //initialize
-        mStartDate = (EditText)findViewById(R.id.startDateEditText);
-        mEndDate = (EditText)findViewById(R.id.endDateEditText);
-        mCalculateBtn = (Button)findViewById(R.id.calculateBtn);
 
-        //listen to when the mCalculateBtn is click
-        mCalculateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //get the start and the end date and convert to
-            }
-        });
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 
+        findViewsById();
 
+        setDateTimeField();
     }
 
+    private void findViewsById() {
+        fromDateEtxt = (EditText) findViewById(R.id.fromDateEditText);
+        fromDateEtxt.setInputType(InputType.TYPE_NULL);
+        fromDateEtxt.requestFocus();
+
+        toDateEtxt = (EditText) findViewById(R.id.toDateEditText);
+        toDateEtxt.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setDateTimeField() {
+        fromDateEtxt.setOnClickListener(this);
+        toDateEtxt.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        toDatePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                toDateEtxt.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,17 +81,11 @@ public class WorkActivity extends Activity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        if(view == fromDateEtxt) {
+            fromDatePickerDialog.show();
+        } else if(view == toDateEtxt) {
+            toDatePickerDialog.show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
